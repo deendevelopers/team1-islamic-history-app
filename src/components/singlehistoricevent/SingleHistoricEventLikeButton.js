56dbbe7be46fafa6b7fdd53708537b2d08ce53f5
Hassan Abbas <habbas@dunelm.com>
2020-01-26 16:06:25 +0000

    share button

--- src/components/singlehistoricevent/SingleHistoricEventLikeButton.js
+++ src/components/singlehistoricevent/SingleHistoricEventLikeButton.js
@@ -1,14 +1,15 @@
-import React, {useState} from 'react';
-import styled from 'styled-components';
-import {FlexColumnFullWidthView} from '../views/FlexView';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FlexColumnFullWidthView } from "../views/FlexView";
 import {
   COLOR_LOVE_HEART,
   EVENT_QUOTE_USER_COLOR,
-  FACT_COLOR,
-} from '../../constants/styles';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faHeart, faSortDown} from '@fortawesome/free-solid-svg-icons';
-import TweenOne from 'rc-tween-one';
+  FACT_COLOR
+} from "../../constants/styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faSortDown } from "@fortawesome/free-solid-svg-icons";
+import TweenOne from "rc-tween-one";
+import ShareButton from "../common/ShareButton";
 
 const View = styled.div`
   display: flex;
@@ -18,7 +19,7 @@ const View = styled.div`
 `;
 
 function SingleHistoricEventLikeButton(props) {
-  const {event, isLiked} = {...props};
+  const { event, isLiked } = { ...props };
   const [updatedLike, setUpdateLke] = useState(null);
 
   console.log("button like", updatedLike, isLiked);
@@ -38,10 +39,16 @@ function SingleHistoricEventLikeButton(props) {
   }
 
   return (
-      <View>
-        <FontAwesomeIcon icon={faHeart} size={'lg'} onClick={onLikeClick} color={getLike() ? COLOR_LOVE_HEART : FACT_COLOR }/>
-        <div>{event.loves}</div>
-      </View>
+    <View>
+      <FontAwesomeIcon
+        icon={faHeart}
+        size={"lg"}
+        onClick={onLikeClick}
+        color={getLike() ? COLOR_LOVE_HEART : FACT_COLOR}
+      />
+      <div>{event.loves}</div>
+      <ShareButton event={(event.description, event.subject)} />
+    </View>
   );
 }
 
