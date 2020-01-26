f284101413e47bf0c136c37aed31774374c13bab
Jawad Khokhar <jawad.kho@gmail.com>
2020-01-26 15:34:11 +0000

    Remove undefined issues – count items

--- src/containers/SingleHistoricEventDetailViewContainer.js
+++ src/containers/SingleHistoricEventDetailViewContainer.js
@@ -1,42 +1,39 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import NavbarContainer from './navbar/NavbarContainer';
 import FlexView from '../components/views/FlexView';
 import historicEventsService from '../services/HistoricEventsService';
-import SingleHistoricEventText
-  from '../components/singlehistoricevent/SingleHistoricEventText';
-import SingleHistoricEventHeader
-  from '../components/singlehistoricevent/SingleHistoricEventHeader';
+import SingleHistoricEventText from '../components/singlehistoricevent/SingleHistoricEventText';
+import SingleHistoricEventHeader from '../components/singlehistoricevent/SingleHistoricEventHeader';
 import RcQueueAnim from 'rc-queue-anim';
-import SingleHistoricEventScrollIndicator
-  from '../components/singlehistoricevent/SingleHistoricEventScrollIndicator';
-import SingleHistoricEventQuoteView
-  from '../components/singlehistoricevent/SingleHistoricEventQuoteView';
+import SingleHistoricEventScrollIndicator from '../components/singlehistoricevent/SingleHistoricEventScrollIndicator';
+import SingleHistoricEventQuoteView from '../components/singlehistoricevent/SingleHistoricEventQuoteView';
 import ScrollableView from '../components/views/ScrollableView';
-import SingleHistoricEventDetailsView
-  from '../components/singlehistoricevent/SingleHistoricEventDetailsView';
+import SingleHistoricEventDetailsView from '../components/singlehistoricevent/SingleHistoricEventDetailsView';
 
 const FactViewWrapper = styled.div`
-display: flex;
-flex-direction: column;
-flex: auto;
-align-items: center;
+  display: flex;
+  flex-direction: column;
+  flex: auto;
+  align-items: center;
 `;
 
 function SingleHistoricEventViewContainer() {
   const [event, setEvent] = useState('');
 
   useEffect(() => {
-    historicEventsService.getOne(Math.round(Math.random() * 10))
-        .then(data => setEvent(data));
+    const items = historicEventsService.getAll().length;
+    historicEventsService
+      .getOne(Math.round(Math.random() * items))
+      .then(data => setEvent(data));
   }, []);
 
   console.log(event);
 
   return (
-      <FlexView flexDirection={'column'} height={'100%'}>
-        <SingleHistoricEventDetailsView/>
-      </FlexView>
+    <FlexView flexDirection={'column'} height={'100%'}>
+      <SingleHistoricEventDetailsView />
+    </FlexView>
   );
 }
 
