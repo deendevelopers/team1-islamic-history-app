31ef51d7cba3a1f05b555bc6d8aa536e728aa92e
mafz <mafzdeveloper@gmail.com>
2020-01-26 11:20:06 +0000

    Navigation and stuff

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:3004";
+export const API_BASE_URL = "http://192.168.49.176:3004";
 export const API_HISTORIC_EVENTS_URL = API_BASE_URL + "/events";
--- src/store/navigation/actions.js
+++ src/store/navigation/actions.js
@@ -11,8 +11,12 @@ export const navigateToSingleHistoricEventDetail = () => ({
   page: view.SINGLE_HISTORIC_EVENT_DETAIL_VIEW,
 });
 
-
 export const navigateToTimelineEvents = () => ({
   type: type.NAVIGATE_TO_SINGLE_HISTORIC_EVENT_DETAIL,
   page: view.TIMELINE_HISTORIC_EVENTS_VIEW,
+});
+
+export const navigateToLikedEvents = () => ({
+  type: type.NAVIGATE_TO_LIKED_EVENTS,
+  page: view.LIKED_HISTORIC_EVENTS_VIEW,
 });
